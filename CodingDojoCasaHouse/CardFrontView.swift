ba429df198c352a5d178db1504409d5bc02f2cf0
gugadgalego <gugadgalego@gmail.com>
2023-08-11 14:29:22 -0300

    Rafinha out

--- CodingDojoCasaHouse/CardFrontView.swift
+++ CodingDojoCasaHouse/CardFrontView.swift
@@ -11,41 +11,47 @@ struct CardFrontView: View {
     var body: some View {
         VStack {
             Text("GUGA BONITO")
+                .padding(.top)
                 .bold()
                 
             Spacer()
             HStack {
                 Text("DELIVERY STAFF")
                     .rotationEffect(Angle(degrees: 270))
-                    .font(.system(size: 10))
+                    .font(.system(size: 8))
                     .bold()
+                    .padding(.leading, -20)
                 Spacer()
                 Image(systemName: "command")
                     .font(.system(size: 45))
-                    .bold()
+                    
                 Spacer()
                 Text("DELIVERY STAFF")
                     .rotationEffect(Angle(degrees: 90))
-                    .font(.system(size: 10))
+                    .font(.system(size: 8))
                     .bold()
+                    .padding(.trailing, -20)
             }
             Spacer()
             HStack {
                 Text("web:")
+                    
                     .bold()
                 Text(verbatim: "www.bonito.com")
-                    .foregroundColor(.black)
-            }
+                    .foregroundColor(Color("Cinza"))
+            }.font(.system(size: 12))
             HStack {
                 Text("tel:")
                     .bold()
                 Text("130-790-570")
-                    .foregroundColor(.black)
-            }
+                    .foregroundColor(Color("Cinza"))
+                    
+            }.padding(.bottom)
+                .font(.system(size: 12))
         }
         .frame(width:350, height:240)
         .background(Color.white.shadow(radius: 3))
-        .foregroundColor(.blue)
+        .foregroundColor(Color("Azul"))
     }
 }
 
