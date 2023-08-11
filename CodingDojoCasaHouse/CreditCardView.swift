2db2d5f7b4bdcd8a44db5b857a5ee30a4abbe083
gugadgalego <gugadgalego@gmail.com>
2023-08-11 16:26:40 -0300

    bonito out

--- CodingDojoCasaHouse/CreditCardView.swift
+++ CodingDojoCasaHouse/CreditCardView.swift
@@ -9,13 +9,35 @@ import SwiftUI
 
 struct CreditCardView: View {
     var body: some View {
-        VStack(spacing: 0) {
+        VStack {
             VStack {
-                
-                
-                Text("Banks are supported")
+                CreditCard()
+                    .border(.red)
+                    .frame(width:300,height: 200)
+                VStack{
+                    Text("Banks are supported")
+                        .font(.title3)
+                        .bold()
+                    Text("Including yours")
+                }
+                .frame(width:300,height:200)
+                .background(.white)
             }
-         }
+            .background(.red)
+            .cornerRadius(20)
+            .shadow(radius:10)
+            Button {
+                
+            } label: {
+                Circle()
+                    .frame(width:60,height:60)
+                    .offset(y:-40)
+            }
+        }
+        
+
+        
+        
     }
 }
 
