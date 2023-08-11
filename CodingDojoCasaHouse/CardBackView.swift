8bf89fbdf22f95e2bc33a380233c3e8177c915dd
gugadgalego <gugadgalego@gmail.com>
2023-08-11 15:00:59 -0300

    Dani out

--- CodingDojoCasaHouse/CardBackView.swift
+++ CodingDojoCasaHouse/CardBackView.swift
@@ -18,9 +18,9 @@ struct CardBackView: View {
             
         }
         .frame(width:350, height:240)
-        .background(LinearGradient(gradient: Gradient(colors: [Color(red: 20/255, green: 39/255, blue: 80/255)]), startPoint: <#T##UnitPoint#>, endPoint: <#T##UnitPoint#>) Color("Azul").shadow(radius: 3))
+        .background(LinearGradient(gradient: Gradient(colors: [Color(red: 20/255, green: 39/255, blue: 80/255), Color(red: 34/255, green: 48/255, blue: 109/255)]), startPoint: .trailing, endPoint: .leading))
         .foregroundColor(Color.white)
-    
+        .background(Color.white.shadow(.drop(radius:5 )))
     }
 }
 
