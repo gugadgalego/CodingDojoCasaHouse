12cebc1bca1a361e53dcfbac7e368ca344aa5799
gugadgalego <gugadgalego@gmail.com>
2023-08-11 14:18:59 -0300

    Bonito out

--- CodingDojoCasaHouse/CardFrontView.swift
+++ CodingDojoCasaHouse/CardFrontView.swift
@@ -11,17 +11,41 @@ struct CardFrontView: View {
     var body: some View {
         VStack {
             Text("GUGA BONITO")
+                .bold()
+                
+            Spacer()
             HStack {
                 Text("DELIVERY STAFF")
+                    .rotationEffect(Angle(degrees: 270))
+                    .font(.system(size: 10))
+                    .bold()
                 Spacer()
                 Image(systemName: "command")
+                    .font(.system(size: 45))
+                    .bold()
                 Spacer()
                 Text("DELIVERY STAFF")
+                    .rotationEffect(Angle(degrees: 90))
+                    .font(.system(size: 10))
+                    .bold()
+            }
+            Spacer()
+            HStack {
+                Text("web:")
+                    .bold()
+                Text(verbatim: "www.bonito.com")
+                    .foregroundColor(.black)
             }
             HStack {
-                Text("web: ")
+                Text("tel:")
+                    .bold()
+                Text("130-790-570")
+                    .foregroundColor(.black)
             }
         }
+        .frame(width:350, height:240)
+        .background(Color.white.shadow(radius: 3))
+        .foregroundColor(.blue)
     }
 }
 
