2a906d2f82918ac533899a014249be47dff3e990
gugadgalego <gugadgalego@gmail.com>
2023-08-11 13:54:23 -0300

    Initial Commit

---
+++ CodingDojoCasaHouse/CodingDojoCasaHouseApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodingDojoCasaHouseApp.swift
+//  CodingDojoCasaHouse
+//
+//  Created by gustavo galego on 11/08/23.
+//
+
+import SwiftUI
+
+@main
+struct CodingDojoCasaHouseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
