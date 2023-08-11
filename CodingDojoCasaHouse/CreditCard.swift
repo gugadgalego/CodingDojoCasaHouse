d9e2f8ec5446a633af376b6c08dc2a3002f8004a
gugadgalego <gugadgalego@gmail.com>
2023-08-11 15:32:15 -0300

    gi out

--- CodingDojoCasaHouse/CreditCard.swift
+++ CodingDojoCasaHouse/CreditCard.swift
@@ -12,24 +12,30 @@ struct CreditCard: View {
         VStack(alignment: .leading) {
             HStack {
                 Image(systemName: "rectangle.split.3x3.fill")
-                    .resizable()
-                    .frame(width: 70, height: <#T##CGFloat?#>)
                 Spacer()
                 Image(systemName: "circlebadge.2.fill")
+                    
             }
+            .font(.system(size: 25))
+            .foregroundColor(.white)
             .padding(.bottom)
             Text("XXXX XXXX XXXX 1234")
                 .padding(.bottom, 5)
+                .foregroundColor(.white)
             Group {
                 Text("02/01")
                     .font(.system(size: 12))
+                    .foregroundColor(.white)
+                    .opacity(0.8)
                 Text("Raphael Iniesta")
+                    .foregroundColor(.white)
+                
             }
             .font(.custom("Myriad", size: 18))
         }
         .padding()
         .frame(width: 250, height: 150)
-        .background(.orange)
+        .background(LinearGradient(gradient: Gradient(colors: [Color(red: 255/255, green: 128/255, blue: 121/255), Color(red: 246/255, green:220/255, blue: 138/255)]), startPoint: .trailing, endPoint: .leading))
         .cornerRadius(10)
     }
 }
