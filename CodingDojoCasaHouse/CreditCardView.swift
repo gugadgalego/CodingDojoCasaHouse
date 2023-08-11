5ffb0472980dcb4d5f63ba88885c39d66dca0e2b
gugadgalego <gugadgalego@gmail.com>
2023-08-11 16:37:28 -0300

    Igor Out

--- CodingDojoCasaHouse/CreditCardView.swift
+++ CodingDojoCasaHouse/CreditCardView.swift
@@ -12,27 +12,31 @@ struct CreditCardView: View {
         VStack {
             VStack {
                 CreditCard()
-                    .border(.red)
                     .frame(width:300,height: 200)
+                    .shadow(radius:15)
                 VStack{
                     Text("Banks are supported")
                         .font(.title3)
                         .bold()
                     Text("Including yours")
                 }
-                .frame(width:300,height:200)
+                .frame(width:300,height:170)
                 .background(.white)
             }
-            .background(.red)
+            .background(LinearGradient(gradient: Gradient(colors: [Color(red: 255/255, green: 128/255, blue: 121/255), Color(red: 246/255, green:220/255, blue: 138/255)]), startPoint: .trailing, endPoint: .leading))
             .cornerRadius(20)
+            .compositingGroup()
             .shadow(radius:10)
             Button {
                 
             } label: {
-                Circle()
+                ZStack {
+                    Circle()
                     .frame(width:60,height:60)
-                    .offset(y:-40)
-            }
+                        .background(LinearGradient(gradient: Gradient(colors: [Color(red: 255/255, green: 128/255, blue: 121/255), Color(red: 246/255, green:220/255, blue: 138/255)]), startPoint: .topTrailing, endPoint: .bottomLeading))
+                    
+                }
+            }.offset(y:-40)
         }
         
 
